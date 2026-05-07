18b71c87e7d80fe05fbf8a327f873c97378fc9a4
Tobiasz <info@tobyshighlandtours.com>
2026-05-07 19:15:33 +0100

    feat: add Google Tag Manager

--- app/app/(site)/layout.tsx
+++ app/app/(site)/layout.tsx
@@ -7,7 +7,30 @@ import GoogleAnalytics from './components/GoogleAnalytics'
 export default function SiteLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-KD77M543');`,
+          }}
+        />
+        {/* End Google Tag Manager */}
+      </head>
       <body style={{ margin: 0 }}>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-KD77M543"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
         <GoogleAnalytics />
         <LanguageProvider>
         <style>{`
